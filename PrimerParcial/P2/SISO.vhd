9e7cc4dc7772a4cf9f677ae437b631c8c8283067
egjimenezg <egjimenezg@gmail.com>
2014-09-02 16:16:37 -0500

    coding #1 agregando registro entrada paralela salida paralela

--- PrimerParcial/P2/SISO.vhd
+++ PrimerParcial/P2/SISO.vhd
@@ -2,27 +2,25 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    19:02:23 09/01/2014 
--- Module Name:    PIPO - Behavioral 
+-- Create Date:    10:52:30 09/01/2014 
+-- Module Name:    SISO - Behavioral 
 -- Author:         Gamaliel Jiménez
-
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity PIPO is Port(
-  clock : in STD_LOGIC;
-  e : in STD_LOGIC_VECTOR(0 to 15);
-  s : out STD_LOGIC_VECTOR (0 to 15)
-);
-end PIPO;
+entity SISO is
+    Port ( clock : in  STD_LOGIC;
+      e : in STD_LOGIC;
+      s : inout STD_LOGIC_VECTOR(0 TO 3));
+end SISO;
 
-architecture Behavioral of PIPO is
-
+architecture Behavioral of SISO is
 begin
-  process(clock,e) begin
+  process(clock,s,e) begin
     if(clock'event and clock='1') then
-      s <= e;
+      s(0) <= e;
+      s(1 to 3) <= s(0 to 2);
     end if;
   end process;
 end Behavioral;
