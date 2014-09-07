4ac0745868dd5582eefa4d8111407acf69d4a15a
egjimenezg <egjimenezg@gmail.com>
2014-09-06 23:59:12 -0500

    coding #1 comenzando registro entrada serie salida paralelo

---
+++ PrimerParcial/P3/SIPO.vhd
@@ -0,0 +1,20 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:52:30 09/06/2014 
+-- Module Name:    SIPO - Behavioral 
+-- Author:         Gamaliel Jiménez
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity SIPO is
+    Port ( clock : in  STD_LOGIC);
+end SIPO;
+
+architecture Behavioral of SIPO is
+begin
+
+end Behavioral;
+
