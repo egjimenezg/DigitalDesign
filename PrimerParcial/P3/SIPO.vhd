1b1a2981319727bcba06e13eb1228473d2104a2a
egjimenezg <egjimenezg@gmail.com>
2014-09-07 03:10:30 -0500

    coding #1 agregando registro entrada serie salida paralela

--- PrimerParcial/P3/SIPO.vhd
+++ PrimerParcial/P3/SIPO.vhd
@@ -10,11 +10,24 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity SIPO is
-    Port ( clock : in  STD_LOGIC);
+  Port (clock : in  STD_LOGIC;
+        e,h : in STD_LOGIC;
+        s : out STD_LOGIC_VECTOR(0 to 3) := "0000");
 end SIPO;
 
 architecture Behavioral of SIPO is
+  signal si : STD_LOGIC_VECTOR(0 to 3) := "0000";
 begin
-
+  process(clock,h,si) begin
+    if(h='0') then
+      if(clock'event and clock='1') then
+        si(0) <= e;
+        si(1 to 3) <= si(0 to 2);
+      end if;
+    elsif (h='1') then
+      s <= si;
+    end if;
+  end process;
 end Behavioral;
+
 
